6de1918213fc890ea9ccd2ae85da3ae974bc43b7
Dakar <kenyibenning11@gmail.com>
2024-11-11 11:34:59 -0500

    Add files via upload

---
+++ proyecto/Productos.java
@@ -0,0 +1,22 @@
+public class Productos {
+    public void bebidas(){
+        System.out.println("");
+        Subtotales subtotales = new Subtotales();
+        subtotales.sub();
+    }
+    public void alimentos(){
+        System.out.println("");
+        Subtotales subtotales = new Subtotales();
+        subtotales.sub();
+    }
+    public void merch(){
+        System.out.println("");
+        Subtotales subtotales = new Subtotales();
+        subtotales.sub();
+    }
+    public void packs(){
+        System.out.println("");
+        Subtotales subtotales = new Subtotales();
+        subtotales.sub();
+    }
+}
---
+++ proyecto/Subtotales.java
@@ -0,0 +1,5 @@
+public class Subtotales {
+    public void sub(){
+        System.out.println("Procesando los sub totales....");
+    }
+}
